c1361fedc95ed38cb240da4b7437aeefd073059e
HyunWoo Lee <hwlee2k@hyunwoo-leeui-mac-mini.local>
2017-02-05 11:53:27 +0900

    Ch2 TestCase

---
+++ Ch02_Values/Ch02_Values.swift
@@ -0,0 +1,27 @@
+//
+//  Ch02_Values.swift
+//  Ch02_Values
+//
+//  Created by  HyunWoo Lee on 2017. 2. 5..
+//  Copyright © 2017년  HyunWoo Lee. All rights reserved.
+//
+
+import XCTest
+
+class Ch02_Values: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testString() {
+        // String Concat.
+        print("Hello, " + "Swift")
+        
+    }
+    
+}
