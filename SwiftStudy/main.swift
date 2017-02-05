aadbe94e729a6b8d2e7ae0a8bd6c296cfe1776f0
HyunWoo Lee <hwlee2k@hyunwoo-leeui-mac-mini.local>
2017-02-05 09:10:31 +0900

    Initial Commit

---
+++ SwiftStudy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftStudy
+//
+//  Created by  HyunWoo Lee on 2017. 2. 5..
+//  Copyright © 2017년  HyunWoo Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
