e4939eba1121ebef2055f612c88c294ffa3642b1
HyunWoo Lee <hwlee2k@hyunwoo-leeui-mac-mini.local>
2017-02-05 12:27:07 +0900

    Ch02_Values Updated

--- Ch02_Values/Ch02_Values.swift
+++ Ch02_Values/Ch02_Values.swift
@@ -19,9 +19,30 @@ class Ch02_Values: XCTestCase {
     }
     
     func testString() {
-        // String Concat.
-        print("Hello, " + "Swift")
+        // 문자열
+        // "안녕, Swift"
         
+        // 빈 문자열
+        // "", String()
+        print("")
+        print(String())
+        
+        // 문자열은 합할 수 있다.
+        print("> 안녕, " + "Swift")
+        
+        // 여러 열(Multi-line) 문자열을 만드는 간편한 방법은 없다.
+        // 단지 +로 더하는 방법을 사용할 수 있다.
+        print(
+            "> 길고 긴... 문자열 \n" +
+            "길고 긴.... 문자열"
+        )
+        
+        // 다른 타입과 합할 수는 없다
+        // print("안녕, " + "Swift" + 3.0)
+        
+        // 먼저 문자열로 변환한 후 합한다.
+        print ("> 안녕, " + "Swift " + String(3))
+        print ("> 안녕, " + "Swift " + String(3.0))
     }
     
 }
